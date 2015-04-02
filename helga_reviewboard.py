d6cc1ad12bfd34da89f62b404809f7b30e3136e1
Jon Robison <jon.robison@coxinc.com>
2015-04-02 14:53:27 -0400

    Add word boundaries for cr

--- helga_reviewboard.py
+++ helga_reviewboard.py
@@ -1,8 +1,9 @@
 from helga import settings
 from helga.plugins import match
 
+REVIEWBOARD_MATCH = r'\bcr(\d+)\b'
 
-@match(r'cr(\d+)')
+@match(REVIEWBOARD_MATCH)
 def reviewboard(client, channel, nick, message, matches):
     """
     Automatically responds to reviewboard urls if a user mentions a pattern
